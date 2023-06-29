1ae2d9024640f3a7e09b5ae6807ebff7159c6549
gamja <gudwns258@gmail.com>
2023-06-29 17:32:47 +0900

    open ec2 server

--- src/main/reactfront/src/components/Request/RequestConfig.js
+++ src/main/reactfront/src/components/Request/RequestConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const RequestConfig = axios.create({
-    baseURL: 'https://api.hjstore.shop', // 백엔드 서버의 URL로 설정합니다.
+    baseURL: 'http://43.202.90.1:9999', // 백엔드 서버의 URL로 설정합니다.
 });
+// https://api.hjstore.shop
 
 export default RequestConfig;
