c73021dcc0885326fc898d71f96ce0104ff8105c
gamja <gudwns258@gmail.com>
2023-07-04 03:14:46 +0900

    delete front directory

--- src/main/reactfront/src/components/Styles/Container/Container.style.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styled from "styled-components";
-
-export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: white;
-`
--- src/main/reactfront/src/components/Styles/Editorform/Editor.style.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import styled from "styled-components";
-
-export const Editor = styled.div`
-  overflow-y: auto;
-  max-height: 200px;
-`
