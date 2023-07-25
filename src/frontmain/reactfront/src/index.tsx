ff33f74d8248bd05f0d392d03ba870c4e08779b8
gamjagogi <110292164+gamjagogi@users.noreply.github.com>
2023-07-25 18:35:29 +0900

    netflty error

--- src/frontmain/reactfront/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-import Counter from "./test/Counter"
-
-ReactDOM.render(<Counter name="React" />, document.getElementById("app"))
