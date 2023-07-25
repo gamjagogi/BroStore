820afbff1b6aa2b38f0fe997088a7d3334f8dd89
gamjagogi <110292164+gamjagogi@users.noreply.github.com>
2023-07-26 02:40:58 +0900

    test

--- src/frontmain/reactfront/src/screens/Home.jsx
+++ src/frontmain/reactfront/src/screens/Home.jsx
@@ -147,7 +147,7 @@ class Home extends Component {
                             </Link>
                         </div>
                         <div className="col-md-3">
-                            <Link to="/" className="text-decoration-none">
+                            <Link to="/my" className="text-decoration-none">
                                 <img
                                     src="../../images/category/smartwatch.webp"
                                     className="img-fluid rounded-circle"
