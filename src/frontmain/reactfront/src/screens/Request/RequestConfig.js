3832ac166dbac805de79e7bb9d960171fb63d36a
gamjagogi <110292164+gamjagogi@users.noreply.github.com>
2023-08-14 17:39:16 +0900

    publishing test

--- src/frontmain/reactfront/src/screens/Request/RequestConfig.js
+++ src/frontmain/reactfront/src/screens/Request/RequestConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const RequestConfig = axios.create({
-    baseURL: 'https://api.hjstore.shop', // 백엔드 서버의 URL로 설정합니다.
+    baseURL: 'https://api.hjstore.shop:9999', // 백엔드 서버의 URL로 설정합니다.
 });
 
 // http://43.201.138.121:9999
